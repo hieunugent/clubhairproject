691f5c692fff5c1adc374e4380ae5e5dc5fba3c5
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-01-04 22:20:19 -0800

    finish ourproduct on landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo_clubhair from "./image/logoCH.png"
+import productimage from "./image/razor.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="top__image"> 
+            <div className="logo_banner">
+                  <img className="image__CH" src={logo_clubhair} alt="logonbanner" width="80%" height="20%"/>
+            </div>
+        </div>
+        <div className="middle__item">
+        <div className="second__section">
+          <h3 className="title__section">OUR SERVICES</h3>
+          <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className="third__sections">
+          <div className="third__section">
+            <h3 className="title__sectionv2">BASIC HAIRCUT</h3>
+            <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+          </div>
+          <div className="third__section">
+          <h3 className="title__sectionv2">FANCY HAIRCUT</h3>
+          <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+          </div>
+          <div className="third__section">
+          <h3 className="title__sectionv2">SHAMPOO </h3>
+          <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+         </div></div>
+         <div className="third__sections">
+          <div className="third__section">
+            <h3 className="title__sectionv2">FULL HAIRCUT</h3>
+            <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+          </div>
+          <div className="third__section">
+          <h3 className="title__sectionv2">SHAPE HAIR</h3>
+          <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+          </div>
+          <div className="third__section">
+          <h3 className="title__sectionv2">BUZZCUT </h3>
+          <p className="description__section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  
+         </div>
+        </div>
+        <button className="appointment__Btn">MAKE AN APPOINMENT</button>
+        </div>
+        <div className="product__sections">
+            <div>
+              <img src={productimage} alt="productimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section">OUR PRODUCTS </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+            <button className="shopping__Btn">START SHOPPING</button>
+
+            </div>
+        </div>
+
+
+   
     </div>
   );
 }
