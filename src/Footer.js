ceacfc3d3bb958e4e5c82f77dbde174b0e3ffe7a
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-01-30 23:15:50 -0800

    add service page

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,23 @@ import React from 'react'
 import "./Footer.css"
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
-
+import { Link } from "react-router-dom";
 function Footer() {
     return (
         <div className="footer">
           
             <h3 > Contact Information </h3>
              <div className="footer__media">
-             <button className="footer__btn--facebook"> <FacebookIcon/> </button>
-             <button className="footer__btn--instagram"> <InstagramIcon/> </button>
+             <button className="footer__btn--facebook"> 
+              <a href="https://www.facebook.com/">
+             <FacebookIcon/>
+             </a>
+             </button>
+             <button className="footer__btn--instagram"> 
+            <a href="https://www.instagram.com/">
+            <InstagramIcon/>
+            </a>
+              </button>
              </div>
            
         </div>
