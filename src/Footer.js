d2b8b44535276c9abb4d769c83ccd52858178294
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-01-30 23:04:10 -0800

    add service page

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./Footer.css"
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 function Footer() {
     return (
         <div className="footer">
-            <h3> Contact Information </h3>
+          
+            <h3 > Contact Information </h3>
              <div className="footer__media">
-             <p> Facebook </p>
-             <p> Instagram</p>
+             <button className="footer__btn--facebook"> <FacebookIcon/> </button>
+             <button className="footer__btn--instagram"> <InstagramIcon/> </button>
              </div>
            
         </div>
--- src/Service.js
+++ src/Service.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo_clubhair from "./image/logoCHnew.png";
+import sampleimage from "./image/sampleImage.png"
 import "./Service.css";
 function Service() {
   return (
@@ -12,12 +13,52 @@ function Service() {
       <div className="middle__item">
         <div className="second__section">
           <h3 className="title__section">OUR SERVICES</h3>
-          <p className="description__section">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
         </div>
       </div>
+      <div className="product__sections">
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section">Service One </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+           
+            </div>
+        </div>
+        <div className="product__sections">
+           
+            <div className="product__description">
+            <h3 className="title__section">Service Two </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+           
+            </div>
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+        </div>
+        <div className="product__sections">
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section">Service Three </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+           
+            </div>
+        </div>
+        <div className="product__sections">
+           
+            <div className="product__description">
+            <h3 className="title__section">Service Four </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+           
+            </div>
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+        </div>
+
+
     </div>
   );
 }
