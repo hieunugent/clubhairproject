ca68d60dc10747faf37d36c99b0d2248e46b2275
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-02-01 21:22:57 -0800

    add Shopping page

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo_clubhair from "./image/logoCHnew.png";
+import sampleimage from "./image/sampleImage.png"
 import "./Product.css";
 function Product() {
     return (
@@ -13,6 +14,33 @@ function Product() {
           <div className="second__section">
             <h3 className="title__section">OUR PRODUCTS</h3>
           </div>
+          <div className="product__sections">
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section"> Product One </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+            </div>
+        </div>
+        <div className="product__sections">
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section"> Product Two </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+            </div>
+        </div>
+        <div className="product__sections">
+            <div>
+              <img className="productimage" src={sampleimage} alt="sampleimage" width="95%"/>
+            </div>
+            <div className="product__description">
+            <h3 className="title__section"> Product Three </h3>
+            <p className="description__product">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus.</p> 
+            </div>
+        </div>
         </div>
       </div>
     )
